2c76ebc504003bc8745f91c565289290d24d9a74
nischay-code <nischay.patel.code@gmail.com>
2024-01-30 09:44:04 +0530

    Creating Sign in/Login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,41 +2,61 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
-
-router.get("/", (req, res) => {
-  res.send("HEY");
-});
+const bcrypt = require("bcryptjs");
 
 router.post("/signup", (req, res) => {
   const { name, email, password } = req.body;
-  {
-    if (!name || !email || !password) {
-      return res.status(422).json({ error: "Please enter all the fields" });
-    }
-    res.json({ message: "Successfully Posted" });
+  if (!name || !email || !password) {
+    return res.status(422).json({ error: "Please enter all the fields" });
   }
   User.findOne({ email: email })
     .then((savedUser) => {
       if (savedUser) {
         return res.status(422).json({ error: "User already exists!" });
       }
-      const user = new User({
-        email,
-        password,
-        name,
-      });
-      user
-        .save()
-        .then((user) => {
-          res.json({ message: "Saved Successfully" });
-        })
-        .catch((err) => {
-          console.log(err);
+      bcrypt.hash(password, 12).then((hashedPassword) => {
+        const user = new User({
+          email,
+          password: hashedPassword,
+          name,
         });
+        user
+          .save()
+          .then((user) => {
+            return res.status(200).json({ message: "Saved Successfully" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     })
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "Please enter all the fields" });
+  }
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "Invalid Credentials!" });
+    }
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          return res.status(200).json({ message: "Successfully Login" });
+        } else {
+          return res.status(422).json({ error: "Invalid Credentials!" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 module.exports = router;
