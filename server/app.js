d46074ea0e487dadc730fcadf3f56e7e449d9155
nischay-code <nischay.patel.code@gmail.com>
2024-02-02 12:48:12 +0530

    Solved CORS Error

--- server/app.js
+++ server/app.js
@@ -2,8 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const { MONGOURI } = require("./keys");
 const PORT = process.env.PORT || 5000;
+app.use(cors());
+mongoose.connect(MONGOURI);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error : ", err);
+});
+require("./models/user");
+require("./models/post");
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 // NOTE : The Skeleton of the database is created by the attributes and this skeleton is named Schema or a Blueprint.
 
@@ -22,22 +40,3 @@ const PORT = process.env.PORT || 5000;
 //   console.log("About");
 //   res.send("About");
 // });
-
-mongoose.connect(MONGOURI);
-
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
-});
-mongoose.connection.on("error", (err) => {
-  console.log("Error : ", err);
-});
-
-require("./models/user");
-require("./models/post");
-app.use(express.json());
-app.use(require("./routes/auth"));
-app.use(require("./routes/post"));
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
